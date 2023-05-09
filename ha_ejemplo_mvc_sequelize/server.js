f5058a3a7c12a3269afd0054f6b0afc382af8bfc
ShisusSandoval <Alberto110500@gmail.com>
2023-05-09 11:11:27 -0700

    a

--- ha_ejemplo_mvc_sequelize/server.js
+++ ha_ejemplo_mvc_sequelize/server.js
@@ -17,3 +17,4 @@ app.listen(APP_PORT, () => {
   console.log(`\n[Express] Servidor corriendo en el puerto ${APP_PORT}.`);
   console.log(`[Express] Ingresar a http://localhost:${APP_PORT}.\n`);
 });
+a;
